72604ab3ad6e70faaa7e2110448c92c41a65c10d
Mohit Maroliya <mmaroliya@gmail.com>
2020-03-25 17:38:44 +0530

    Lifting dishes to App Comp and using as props in Menu Comp

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent.js';
+import { DISHES } from './shared/dishes';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color = "primary">
-        <div className="container">
-          <NavbarBrand href="/">
-            Mohit Da Dhabaa !
-          </NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+
+class App extends Component {
+  
+  constructor(props){
+    super(props);
+
+    this.state = { dishes: DISHES};  // Added dishes as lifted component to App
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color = "primary">
+          <div className="container">
+            <NavbarBrand href="/">
+              Mohit ka Fusion Dhabaa !
+            </NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes = {this.state.dishes}/>  {/*Menu : child and App: parent => availablr to props to menucomponent */}
+      </div>
+    );
+  }
 }
 
 export default App;
