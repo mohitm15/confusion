48d57eb80a0d2a897e78d6433801a4fd45514db0
Mohit Maroliya <mmaroliya@gmail.com>
2020-03-31 13:02:58 +0530

    Presentational and Container components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-
-import {Navbar, NavbarBrand} from 'reactstrap';
-import Menu from './components/MenuComponent.js';
-import { DISHES } from './shared/dishes';
-
+import Menu from './components/MainComponent';
 
 class App extends Component {
   
-  constructor(props){
-    super(props);
-
-    this.state = { dishes: DISHES};  // Added dishes as lifted component to App
-  }
-
   render() {
     return (
       <div className="App">
-        <Navbar dark color = "primary">
-          <div className="container">
-            <NavbarBrand href="/">
-              Mohit ka Fusion Dhabaa !
-            </NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes = {this.state.dishes}/>  {/*Menu : child and App: parent => availablr to props to menucomponent */}
+        <Menu /> 
       </div>
     );
   }
