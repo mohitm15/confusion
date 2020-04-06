3315cdfe6245f5da824411f8dd8d203447610d22
Mohit Maroliya <mmaroliya@gmail.com>
2020-04-06 14:03:31 +0530

    Added controlled form for feedback

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,49 +1,157 @@
-import React from 'react';
-import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from 'react';
+import { Breadcrumb, BreadcrumbItem, Button, Form, FormGroup, Label, Col, Input } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-function Contact(props) {
-    return(
-        <div className="container">
-            <div className="row">
-                <Breadcrumb>
-                    <BreadcrumbItem>
-                        <Link to='/home'>Home</Link>
-                    </BreadcrumbItem>
-                    <BreadcrumbItem active>
-                        <Link to='/contactus'>Contact Us</Link>
-                    </BreadcrumbItem>
-                </Breadcrumb>
-            </div>
-            <div className="col-12">
-                <h3>Contact Us</h3><hr />
-            </div>
-            <div className="row row-content">
+class Contact extends Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            firstname: '',
+            lastname: '',
+            telnum: '',
+            email: '',
+            agree: false,
+            contactType: 'Tel.',
+            message: ''
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+        // binding the functn with constructor
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        })
+    }
+    
+    handleSubmit(event){
+        console.log("Current STate :" + JSON.stringify(this.state));
+        alert("Current State :" + JSON.stringify(this.state));
+        event.preventDefault();
+        
+    }
+    render() {
+        return(
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem>
+                            <Link to='/home'>Home</Link>
+                        </BreadcrumbItem>
+                        <BreadcrumbItem active>
+                            <Link to='/contactus'>Contact Us</Link>
+                        </BreadcrumbItem>
+                    </Breadcrumb>
+                </div>
                 <div className="col-12">
-                    <h3>Location Information</h3>
+                    <h3>Contact Us</h3><hr />
                 </div>
-                <div className="col-12 col-sm-4 offset-sm-1">
-                <h5>Our Address</h5>
-                    <address style={{ color:'black'}}>
-		              A-22, Subhash Road<br />
-		              Subhash Nagar, Bhopal, MP<br />
-		              INDIA<br />
-		              <i className="fa fa-phone fa-lg"></i>: +91 98931 92614<br />
-		              <i className="fa fa-fax fa-lg"></i>: (0755) 277 772<br />
-		              <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:mmaroliya@gmail.com">
-                      mohitkadhabba@food.net</a>
-                    </address>
+                <div className="row row-content">
+                    <div className="col-12">
+                        <h3>Location Information</h3>
+                    </div>
+                    <div className="col-12 col-sm-4 offset-sm-1">
+                    <h5>Our Address</h5>
+                        <address style={{ color:'black'}}>
+                          A-22, Subhash Road<br />
+                          Subhash Nagar, Bhopal, MP<br />
+                          INDIA<br />
+                          <i className="fa fa-phone fa-lg"></i>: +91 98931 92614<br />
+                          <i className="fa fa-fax fa-lg"></i>: (0755) 277 772<br />
+                          <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:mmaroliya@gmail.com">
+                          mohitkadhabba@food.net</a>
+                        </address>
+                    </div>
+                    <div className="col-12 col-sm-11 offset-sm-1">
+                        <div className="btn-group" role="group">
+                            <a role="button" className="btn btn-primary" href="tel:+91 9893192614"><i className="fa fa-phone"></i> Call</a>
+                            <a role="button" className="btn btn-info" href="/contactus"><i className="fa fa-skype"></i> Skype</a>
+                            <a role="button" className="btn btn-success" href="mailto:mmaroliya@gmail.com"><i className="fa fa-envelope-o"></i> Email</a>
+                        </div>
+                    </div>
                 </div>
-                <div className="col-12 col-sm-11 offset-sm-1">
-                    <div className="btn-group" role="group">
-                        <a role="button" className="btn btn-primary" href="tel:+91 9893192614"><i className="fa fa-phone"></i> Call</a>
-                        <a role="button" className="btn btn-info" href="/contactus"><i className="fa fa-skype"></i> Skype</a>
-                        <a role="button" className="btn btn-success" href="mailto:mmaroliya@gmail.com"><i className="fa fa-envelope-o"></i> Email</a>
+                <div className="row row-content">
+                    <div className="col-12">
+                        <h3>Send Us Your Feedback</h3>
+                    </div>
+                    <div className="col-12 col-md-9">
+                        <Form onSubmit={this.handleSubmit}>
+                            <FormGroup row>
+                                <Label htmlFor="firstname" md={2}>First Name</Label>
+                                <Col md={10}>
+                                    <Input type="text" id="firstname" name="firstname"
+                                     placeholder="Enter First Name"
+                                    value = {this.state.handleInputChange} />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="lastname" md={2}>Last Name</Label>
+                                <Col md={10}>
+                                    <Input type="text" id="lastname" name="lastname"
+                                     placeholder="Enter Last Name"
+                                    value = {this.state.handleInputChange} />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="telnum" md={2}>Contact No.</Label>
+                                <Col md={10}>
+                                    <Input type="tel" id="telnum" name="telnum"
+                                     placeholder="Contact No."
+                                    value = {this.state.handleInputChange} />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="email" md={2}>Email</Label>
+                                <Col md={10}>
+                                    <Input type="email" id="email" name="email"
+                                     placeholder="Email"
+                                    value = {this.state.handleInputChange} />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Col md={{size: 6, offset:2}}>
+                                    <FormGroup check>
+                                        <Label check>
+                                            <Input type="checkbox" name="agree" value={this.state.handleInputChange}/> {' '}
+                                            <strong>May we contact you !</strong>
+                                        </Label>
+                                    </FormGroup>
+                                </Col>
+                                <Col md={{size: 3, offset:1}}>
+                                    <Input type="select" name="contactTtpe" value={this.state.handleInputChange}>
+                                        <option>Mobile</option>
+                                        <option>Email</option>
+                                    </Input>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="message" md={2}>Your Feedback</Label>
+                                <Col md={10}>
+                                    <Input type="textarea" id="message" name="message" rows="12"
+                                    value = {this.state.handleInputChange} />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Col md={{size:10 , offset:2}}>
+                                    <Button type="submit" color="success">
+                                        Send Feedback
+                                    </Button>
+                                </Col>
+                            </FormGroup>
+                        </Form>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Contact;
