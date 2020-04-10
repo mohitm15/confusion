30e9bcbb310227c6cf7f95a321c8e62ed1dcf6d6
Mohit Maroliya <mmaroliya@gmail.com>
2020-04-10 11:29:55 +0530

    Minor change: Changing the loading icon

--- src/components/LoadingComponent.js
+++ src/components/LoadingComponent.js
@@ -3,7 +3,8 @@ import React from 'react';
 export const Loading = () => {
     return(
         <div className="col-12">
-            <span className="fa fa-spinner fa-pulse fa-3x fa-fw text-primary"></span>
+            <span className="fa fa-cog fa-spin fa-3x fa-fw text-success"></span>
+            <br /><br />
             <p>Loading . . .</p>
         </div>
     );
